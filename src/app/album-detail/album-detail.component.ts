b4b10ecf5dca94d55785f89cbf5605ae0da3a4e4
Guest User <Guest@epicodus-3.local>
2018-04-02 20:01:24 -0700

    connect application to firebase

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -2,22 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'; //required for dynamic routing
 import { Location } from '@angular/common'; //normalizes paths
 import { Album } from '../album.model';//we most import this model to reference it
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
-  albumId: number = null; //will be redefined later by the id passed from the URL
+  albumId: number; //will be redefined later by the id passed from the URL
+  albumToDisplay: Album;
 
-  constructor(private route: ActivatedRoute, private location: Location){}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private albumService: AlbumService
+  ){}
 
   //component lifecyle hook
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.albumId = parseInt(urlParameters['id']);
     });
+    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
 
 }
