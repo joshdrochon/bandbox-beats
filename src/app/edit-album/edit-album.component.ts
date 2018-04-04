4c3395389f327dccfe5293b24c5a52f6d471d724
Guest User <Guest@epicodus-3.local>
2018-04-03 17:19:58 -0700

    add delete method to delete individual entries in the db

--- src/app/edit-album/edit-album.component.ts
+++ src/app/edit-album/edit-album.component.ts
@@ -1,17 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-edit-album',
   templateUrl: './edit-album.component.html',
-  styleUrls: ['./edit-album.component.css']
+  styleUrls: ['./edit-album.component.css'],
+  providers: [AlbumService]
 })
 export class EditAlbumComponent implements OnInit {
   @Input() selectedAlbum;
 
-  constructor() { }
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit() {
+  }
+
+  beginUpdatingAlbum(albumToUpdate){
+    this.albumService.updateAlbum(albumToUpdate);
+  }
+
+  beginDeletingAlbum(albumToDelete){
+    if(confirm("Are you sure you want to remove this item completely?")){
+      this.albumService.deleteAlbum(albumToDelete);
+    }
   }
 
 }
